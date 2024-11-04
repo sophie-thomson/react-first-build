0e6f19697bd42cda1c3657e7594b07913939e71e
Sophie Thomson <sophielbenoit@gmail.com>
2024-11-04 15:26:47 +0000

    Add a stateful class component greeting

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className="App">
       <FunctionalGreeting />
-      <FunctionalGreetingWithProps />
+      {/* greeting 'prop' is added to the FunctionalGreetingWithProps  */}
+      <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Sophie" age="47" />
+      <StatefulGreeting greeting="I'm a stateful class component!"/>
     </div>
   );
 }
--- my-app/src/components/FunctionalGreetingWithProps.js
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 // Arrow function below does same as vanilla function above
-const FunctionalGreetingWithProps = () => <h1>Hello with Props!</h1>;
+const FunctionalGreetingWithProps = (props) => {
+    console.log(props);
+    return <h1>Hello, {props.name}. {props.greeting} How does it feel to be {props.age}?</h1>;
+}
 
 export default FunctionalGreetingWithProps;
