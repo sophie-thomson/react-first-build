01e605b2e0e4e0a70899ba542e2733fcdcbca046
Sophie Thomson <sophielbenoit@gmail.com>
2024-11-04 13:48:11 +0000

    Add greetings components to App.js

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// function FunctionalGreeting() {
+//     return <h1>Hello from React!</h1>
+// }
+
+// Arrow function below does same as vanilla function above
+const FunctionalGreeting = () => <h1>Hello again from React!</h1>;
+
+export default FunctionalGreeting;
---
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+// Arrow function below does same as vanilla function above
+const FunctionalGreetingWithProps = () => <h1>Hello with Props!</h1>;
+
+export default FunctionalGreetingWithProps;
