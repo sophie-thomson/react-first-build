3f4b98ad12f6dbf35a8e5e066feb88dbd7fff037
Sophie Thomson <sophielbenoit@gmail.com>
2024-11-04 17:45:50 +0000

    Add StatefulGreetingWithPrev to change previous state or props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 import StatefulGreeting from './components/StatefulGreeting';
+import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+
 
 function App() {
   return (
@@ -12,6 +15,8 @@ function App() {
       {/* greeting 'prop' is added to the FunctionalGreetingWithProps  */}
       <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Sophie" age="47" />
       <StatefulGreeting greeting="I'm a stateful class component!"/>
+      <StatefulGreetingWithCallback />
+      <StatefulGreetingWithPrevState />
     </div>
   );
 }
--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -1,10 +1,30 @@
 import React from "react";
 
 class StatefulGreeting extends React.Component {
-    render() {
-        return <h1>Hello, {this.props.greeting}
-        </h1>
+    // 'constructor' defines the initial state content to be rendered
+    constructor(props) {
+        super(props);
+        this.state = {
+            introduction: "Hello!",
+            buttonText: "Exit",
+        };
+    };
+
+    handleClick () {
+        this.setState({
+            introduction: "Goodbye!",
+            buttonText: "Enter",
+        });
     }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.introduction} {this.props.greeting}</h1>
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+            </div>
+        )
+    };
 }
 
 export default StatefulGreeting;
