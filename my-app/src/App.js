73f312b6e0fc32e25e80344dc173c85ed05174b4
Sophie Thomson <sophielbenoit@gmail.com>
2024-11-11 16:28:12 +0000

    Add Rendering Lists component with booklist example

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,23 @@
 // eslint-disable-next-line
 import logo from './logo.svg';
 import './App.css';
-import FunctionalGreeting from './components/FunctionalGreeting';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
-import StatefulGreeting from './components/StatefulGreeting';
-import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
-import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
-import EventsFunctional from './components/EventsFunctional';
-import EventsClass from './components/EventsClass';
-import ConditionalRenderingClass from './components/ConditionalRenderingClass';
-import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
-import NestingComponents from './components/NestingComponents';
-import MethodsAsPropsParent from './components/MethodsAsPropsParent';
+// import FunctionalGreeting from './components/FunctionalGreeting';
+// import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+// import StatefulGreeting from './components/StatefulGreeting';
+// import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+// import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+// import EventsFunctional from './components/EventsFunctional';
+// import EventsClass from './components/EventsClass';
+// import ConditionalRenderingClass from './components/ConditionalRenderingClass';
+// import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
+// import NestingComponents from './components/NestingComponents';
+// import MethodsAsPropsParent from './components/MethodsAsPropsParent';
+import RenderingLists from './components/RenderingLists';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting />
+      {/* <FunctionalGreeting /> */}
       {/* greeting 'prop' is added to the FunctionalGreetingWithProps  */}
       {/* <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Sophie" age="47" />
       <StatefulGreeting greeting="I'm a stateful class component!"/>
@@ -26,8 +27,9 @@ function App() {
       <EventsClass />
       <ConditionalRenderingClass />
       <ConditionalRenderingFunctional connected={true} />
-      <NestingComponents /> */}
-      <MethodsAsPropsParent />
+      <NestingComponents /> 
+      <MethodsAsPropsParent /> */}
+      <RenderingLists />
     </div>
   );
 }
