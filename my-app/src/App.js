02ced7e24b63087d543e3acf6f0dce4a8ded0241
Sophie Thomson <sophielbenoit@gmail.com>
2024-11-11 15:39:51 +0000

    Add MethodsAsProps components with Child and Parent ecamples

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,13 +11,14 @@ import EventsClass from './components/EventsClass';
 import ConditionalRenderingClass from './components/ConditionalRenderingClass';
 import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
 import NestingComponents from './components/NestingComponents';
+import MethodsAsPropsParent from './components/MethodsAsPropsParent';
 
 function App() {
   return (
     <div className="App">
       <FunctionalGreeting />
       {/* greeting 'prop' is added to the FunctionalGreetingWithProps  */}
-      <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Sophie" age="47" />
+      {/* <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Sophie" age="47" />
       <StatefulGreeting greeting="I'm a stateful class component!"/>
       <StatefulGreetingWithCallback />
       <StatefulGreetingWithPrevState />
@@ -25,7 +26,8 @@ function App() {
       <EventsClass />
       <ConditionalRenderingClass />
       <ConditionalRenderingFunctional connected={true} />
-      <NestingComponents />
+      <NestingComponents /> */}
+      <MethodsAsPropsParent />
     </div>
   );
 }
